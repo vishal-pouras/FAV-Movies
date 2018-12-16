a9d0cfd4058844b0fc1804e98f8b85af402deee2
vishal-pouras <vishalpouras@gmail.com>
2018-12-16 18:42:41 +0530

    Inital Commit

---
+++ entertaintment_center.py
@@ -0,0 +1,7 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.",
+                        "https://www.imdb.com/title/tt0114709/mediaviewer/rm3813007616",
+                        "https://www.youtube.com/watch?v=P9-jf9-c9JM")
+print(toy_story.poster_image_url)
