1e976885ad85b267cec750c442134f2bfe5c094f
vishal-pouras <vishalpouras@gmail.com>
2018-12-16 19:09:58 +0530

    Added another instance

--- entertaintment_center.py
+++ entertaintment_center.py
@@ -2,6 +2,13 @@ import media
 
 toy_story = media.Movie("Toy Story",
                         "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.",
-                        "https://www.imdb.com/title/tt0114709/mediaviewer/rm3813007616",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=P9-jf9-c9JM")
-print(toy_story.poster_image_url)
+
+print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+print(avatar.title)
+avatar.show_trailer()
